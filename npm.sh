50431b62387b1b9ecca9d632bd4feab4a09ec67b
Travis Fischer <fisch0920@gmail.com>
2015-07-11 00:26:46 -0400

    .

--- npm.sh
+++ npm.sh
@@ -10,6 +10,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 npm update
 
+# install useful command-line tools
 npm install -g gulp
 npm install -g bower
 npm install -g standard
@@ -17,3 +18,28 @@ npm install -g less
 npm install -g tape
 npm install -g castnow
 
+# install some of the most common node dependencies globally just in case i'm 
+# on a remote island without internet and need to hack some codez
+npm install -g request
+npm install -g async
+npm install -g cheerio
+npm install -g express
+npm install -g browserify
+npm install -g underscore
+npm install -g mongodb
+npm install -g mongoose
+npm install -g redis
+npm install -g chalk
+npm install -g colors
+npm install -g q
+npm install -g jshint
+npm install -g bcrypt
+npm install -g glob
+npm install -g http-server
+npm install -g phantomjs
+npm install -g rimraf
+npm install -g inherits
+npm install -g extends
+npm install -g moment
+npm install -g debug
+npm install -g once
